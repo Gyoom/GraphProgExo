6a4d45682ffcee0f178f4c19fdde6ffaef84fe31
Guillaume P <guillaumepiette16@gmail.com>
2024-12-18 02:04:43 +0100

    shield

--- Assets/SpawnShieldRipples.cs
+++ Assets/SpawnShieldRipples.cs
@@ -8,9 +8,23 @@ public class SpawnShieldRipples : MonoBehaviour
     public GameObject shieldRipples;
     private VisualEffect shieldRipplesVFX;
 
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("AAA");
+            var ripples = Instantiate(shieldRipples, transform);
+
+            shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
+            //shieldRipplesVFX.SetVector3("SphereCenter", collision.contacts[0].point);
+
+            Destroy(ripples, 2);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        var ripples = Instantiate(shieldRipples, transform) as GameObject;
+        var ripples = Instantiate(shieldRipples, transform);
 
         shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
         shieldRipplesVFX.SetVector3("SphereCenter", collision.contacts[0].point);
