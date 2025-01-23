5a5db523bcc99882afc3dcca1e68d20c1d31f01e
Guillaume P <guillaumepiette16@gmail.com>
2025-01-23 02:38:11 +0100

    tonight

--- Assets/Characters/PlayerController.cs
+++ Assets/Characters/PlayerController.cs
@@ -2,12 +2,19 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.SceneView;
+using UnityEngine.VFX;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Shield")]
+    public GameObject vfxShield;
+
     ThirdPersonController Controller;
     Animator animator;
+
+    bool shieldActive = false;
+    bool canHit = true;
 
     void Start()
     {
@@ -25,6 +32,37 @@ public class PlayerController : MonoBehaviour
             Controller.CanMove = false;
 
         }
-     
+
+        if (Input.GetKeyDown(KeyCode.E) && vfxShield)
+        {     
+            vfxShield.SetActive(!shieldActive);
+            shieldActive = !shieldActive;
+            canHit = false;
+            StartCoroutine(DelayHit());
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("SwordAttack");
+        }
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Shield") &&  canHit)
+        {
+            Debug.Log("AAA");
+            hit.transform.GetComponent<SpawnShieldRipples>().DisplayRipples(hit.point);
+
+            canHit = false;
+            StartCoroutine(DelayHit());
+        }
+    }
+
+    IEnumerator DelayHit() {
+        yield return new WaitForSeconds(0.5f);
+
+        canHit = true;
     }
 }
--- Assets/Shaders/6_Shield/SpawnShieldRipples.cs
+++ Assets/Shaders/6_Shield/SpawnShieldRipples.cs
@@ -6,28 +6,44 @@ using UnityEngine.VFX;
 public class SpawnShieldRipples : MonoBehaviour
 {
     public GameObject shieldRipples;
+    
+    [SerializeField]
+    private GameObject himself;
+
     private VisualEffect shieldRipplesVFX;
 
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("AAA");
+        /*if (Input.GetKeyDown(KeyCode.Space)) {
             var ripples = Instantiate(shieldRipples, transform);
 
             shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
             //shieldRipplesVFX.SetVector3("SphereCenter", collision.contacts[0].point);
 
             Destroy(ripples, 2);
-        }
+        }*/
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject != himself)
+        {
+            Debug.Log(collision.gameObject.ToString());
+            var ripples = Instantiate(shieldRipples, transform);
+
+            shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
+            shieldRipplesVFX.SetVector3("SphereCenter", collision.contacts[0].point);
+
+            Destroy(ripples, 2);
+        }
+    }
+
+    public void DisplayRipples(Vector3 contactPoint) {
         var ripples = Instantiate(shieldRipples, transform);
 
         shieldRipplesVFX = ripples.GetComponent<VisualEffect>();
-        shieldRipplesVFX.SetVector3("SphereCenter", collision.contacts[0].point);
+        shieldRipplesVFX.SetVector3("SphereCenter", contactPoint);
 
         Destroy(ripples, 2);
     }
